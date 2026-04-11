7c613eb94a7ad1d156c8e8c0464e8e70a4ef3d79
kashaf khan <mdkashaf9931@gmail.com>
2026-04-11 12:32:16 +0530

    added shopEasy logo and update navigation styling and colors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className=" pl-12 pt-4 pr-12 items-center flex gap-10 justify-between">
-      <p className="text-3xl ml-8">shopEasy</p>
-      <label className="border-black border rounded-2xl w-[38%]  p-1">
+    <div className=" pl-12 pt-4 pr-12 items-center flex gap-10 justify-between pb-4 shadow-lg">
+      <img src="/img/shopEasy.png" alt="logo" className="w-40" />
+      <label className="border-gray-500 border  rounded-2xl w-[38%] p-1">
         <input
           className="rounded-2xl w-[95%] pl-5 outline-none"
           type="text"
           placeholder="Search"
         />
       </label>
-      <ul className="mr-8 flex gap-10 list-none cursor-pointer ">
-        <li className="hover:text-blue-500">Home</li>
-        <li className="hover:text-blue-500">Laptop</li>
-        <li className="hover:text-blue-500">Mobile</li>
-        <li className="hover:text-blue-500">Tablet</li>
-        <li className="hover:text-blue-500">Headphone</li>
-        <li className="hover:text-blue-500">Cart</li>
+      <ul className="mr-8 flex gap-10 list-none cursor-pointer  ">
+        <li className="hover:text-cyan-700">Home</li>
+        <li className="hover:text-cyan-700">Laptop</li>
+        <li className="hover:text-cyan-700">Mobile</li>
+        <li className="hover:text-cyan-700">Tablet</li>
+        <li className="hover:text-cyan-700">Headphone</li>
+        <li className="hover:text-cyan-700">Cart</li>
       </ul>
     </div>
   );
