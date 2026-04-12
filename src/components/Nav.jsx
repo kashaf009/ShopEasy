f36a1413829df0964df7a4ca96827ac2f0bfe214
kashaf khan <mdkashaf9931@gmail.com>
2026-04-12 23:00:18 +0530

    updated font

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "@fontsource/manrope";
 
 const Nav = () => {
   return (
-    <div className=" pl-12 pt-4 pr-12 items-center flex gap-10 justify-between pb-4 shadow-lg">
+    <div className=" pl-12 pt-4 pr-12 blackfrop-blur-xl items-center flex gap-10 justify-between pb-4 shadow-sm">
       <img src="/img/shopEasy.png" alt="logo" className="w-40" />
       <label className="border-gray-500 border  rounded-2xl w-[38%] p-1">
         <input
@@ -11,13 +12,23 @@ const Nav = () => {
           placeholder="Search"
         />
       </label>
-      <ul className="mr-8 flex gap-10 list-none cursor-pointer  ">
-        <li className="hover:text-cyan-700">Home</li>
-        <li className="hover:text-cyan-700">Laptop</li>
-        <li className="hover:text-cyan-700">Mobile</li>
-        <li className="hover:text-cyan-700">Tablet</li>
-        <li className="hover:text-cyan-700">Headphone</li>
-        <li className="hover:text-cyan-700">Cart</li>
+      <ul className="mr-8 flex gap-7  list-none cursor-pointer  ">
+        <li className="hover:text-cyan-700 text-slate-700">Home</li>
+        <li className="hover:text-cyan-700 font-['manrope'] text-slate-700">
+          Laptop
+        </li>
+        <li className="hover:text-cyan-700 font-['manrope'] text-slate-700">
+          Mobile
+        </li>
+        <li className="hover:text-cyan-700 font-['manrope'] text-slate-700">
+          Tablet
+        </li>
+        <li className="hover:text-cyan-700 font-['manrope'] text-slate-700">
+          Headphone
+        </li>
+        <li className="hover:text-cyan-700 font-['manrope'] text-slate-700">
+          Cart
+        </li>
       </ul>
     </div>
   );
