368da345bcaef07d6b48bb03b047a58ff6b1f32d
kashaf khan <mdkashaf9931@gmail.com>
2026-04-13 22:29:31 +0530

    build home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import "@fontsource/manrope";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-[40%_60%] mt-35  ">
-      <section>
-        <p>The New Standard</p>
+    <div className="grid grid-cols-[40%_60%] mt-50  ">
+      <section className="pl-8 mt-15 ">
+        <p className="uppercase text-md mb-7 tracking-[0.2em] font-['manrope'] font-semibold text-gray-600 ">
+          The New Standard
+        </p>
+
+        <h1 className="uppercase text-8xl font-['manrope'] font-bold tracking-wide">
+          apple
+          <br />
+          <span className="text-slate-500 ">m5 pro</span>
+        </h1>
+
+        <p className="text-slate-700 font-medium text-lg font-['manrope'] mt-7 w-[85%]">
+          Engineered for absolute performance. Featuring the M5 Silicon
+          architecture and a Liquid Retina XDR display that redefines clarity.
+        </p>
       </section>
       <section>
         <img
-          className="w-[90%] h-[80%] object-cover mx-auto  rounded-3xl"
-          src="https://lh3.googleusercontent.com/aida-public/AB6AXuCB8hjMp9-PurP9ha5o--nAEilssWGr2wn3WyxLawUfBhTsBGLqaNOYcwawawN4A0WgKbWUeaeb979euHgMHpSjeRXBG0UmlS7sXUDJyFQ9AFz1seV8QKVNtp2KYwN9t6caLwzAjStGWP1u1Em0yjATmjbSLgFNgpL7-ejtPvhwCq3-GMWCHyGmvAMr5Rv-h68MCF2HQ25Q5I5Rw8JDvNMvFpf-4lzx1LLDKntrbL6ShfHBHn5J5RoKFLlNw4fK52YiZnz_oXpo-ok"
-          alt=""
+          className="w-[90%] h-full object-cover mx-auto  rounded-3xl"
+          src="https://static0.pocketlintimages.com/wordpress/wp-content/uploads/2023/02/apple-mac-tips-10.jpg?q=49&fit=crop&w=750&dpr=2"
+          alt="laptop image"
         />
       </section>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,36 +3,36 @@ import "@fontsource/manrope";
 
 const Nav = () => {
   return (
-    <div className=" pl-8 pt-4 pr-12 blackfrop-blur-xl items-center flex gap-10 justify-around pb-4 shadow-sm">
+    <div className="fixed z-40 top-0 w-full pl-8 pt-5 pr-12 blackfrop-blur-xl items-center flex gap-10 mx-auto justify-between pb-5 shadow-sm">
       {/* <img src="/img/shopEasy.png" alt="logo" className="w-40" /> */}
       <p className="font-['manrope'] font-bold text-slate-900 tracking-tighter text-2xl">
         SHOPEASY
       </p>
 
-      <ul className="mr-8 flex gap-7  list-none cursor-pointer  ">
-        <li className="hover:text-black text-sm  text-slate-500 font-['manrope'] ">
+      <ul className="  ml-8 flex gap-7  list-none cursor-pointer  ">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium text-sm  text-slate-500 font-['manrope'] ">
           Home
         </li>
-        <li className="hover:text-black text-sm font-['manrope'] text-slate-500">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium text-sm font-['manrope'] text-slate-500">
           Laptop
         </li>
-        <li className="hover:text-black text-sm font-['manrope'] text-slate-500">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium text-sm font-['manrope'] text-slate-500">
           Mobile
         </li>
-        <li className="hover:text-black text-sm font-['manrope'] text-slate-500">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium  text-sm font-['manrope'] text-slate-500">
           Tablet
         </li>
-        <li className="hover:text-black text-sm font-['manrope'] text-slate-500">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium text-sm font-['manrope'] text-slate-500">
           Headphone
         </li>
-        <li className="hover:text-black text-sm font-['manrope'] text-slate-500">
+        <li className="hover:text-slate-900 transition-all duration-300 tracking-tight font-medium text-sm font-['manrope'] text-slate-500">
           Cart
         </li>
       </ul>
 
       <label className="border-gray-500 border bg-gray-100 rounded-2xl w-[15%] p-1">
         <input
-          className="rounded-2xl w-[80%] bg-gra pl-5 outline-none"
+          className="rounded-2xl w-[80%] bg-gray-100 pl-5 outline-none"
           type="text"
           placeholder="Search"
         />
